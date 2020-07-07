0bdf6bd3c6fc9529a5fb75b9bc98d154984b8a59
werfwes <qazxcdewsswedcxzaq12321@gmail.com>
2020-07-07 17:08:40 +0700

    07072020_1708

--- Tester/doTest.xaml.cs
+++ Tester/doTest.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Windows;
 using System.Security.Cryptography;
+using System.Windows.Controls;
 
 namespace Tester
 {
@@ -17,17 +18,22 @@ namespace Tester
         {
             
             InitializeComponent();
-            tp = Class1.testPath;
-            parse();
+            ttrToAQ(Class1.testPath);
+            AQtoTrueAnsw();
+            AQtoTextBoxes();
             label.Content = "№1";
-            textQ.Text = AQ[1, 0];
         }
-        
+
+       
+
         public static string[,] AQ = new string[2, 1000];
         public static string[] A = new string[1000];
+        public static string[] trueAnsw = new string[1000];
         int qNum = 0;
         public static float qCount = 0;
-        public void parse()
+
+
+        public void ttrToAQ(string testPath)
         {
             int i = 0;
             int i1 = 0;
@@ -35,7 +41,7 @@ namespace Tester
 
 
 
-            FileStream stream = new FileStream(tp, FileMode.Open, FileAccess.Read);                              //
+            FileStream stream = new FileStream(testPath, FileMode.Open, FileAccess.Read);                              //
             DESCryptoServiceProvider cryptic = new DESCryptoServiceProvider();                                   //
                                                                                                                  //
             cryptic.Key = ASCIIEncoding.ASCII.GetBytes("ABCDEFGH");                                              // Создание потока дешифратора,
@@ -81,31 +87,188 @@ namespace Tester
 
             crStream.Close();
             stream.Close();
+
+
         }
+        public void AQtoTrueAnsw()
+        {
+            string thisas = "";
+            for(int i = 0; i < qCount; i++)
+            {
+                if (AQ[0, i][0] == '@')
+                {
+                    thisas = AQ[0, i];
+                    string[] As = thisas.Split('$');
+                    foreach (string a in As)
+                    {
+                        if (a[0] == '~')
+                        {
+                            trueAnsw[i] = i.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    trueAnsw[i] = AQ[0, i];
+                }
+            }
+        }
+        public void AQtoTextBoxes()
+        {
+            if (AQ[0, 0][0] == '@')
+            {
+                textA.Visibility = Visibility.Hidden;
+                textQ.Text = AQ[1, 0];
+                int i = 0;
+                string[] As = AQ[0, 0].Split('$');
+                foreach (string s in As)
+                {                   
+                    switch (i + 1)
+                    {
+                        case 1:
+                            a1.Content = s.Trim('~', '@');
+                            rb1.Visibility = Visibility.Visible;
+                            break;
+                        case 2:
+                            a2.Content = s.Trim('~', '@');
+                            rb2.Visibility = Visibility.Visible;
+                            break;
+                        case 3:
+                            a3.Content = s.Trim('~', '@');
+                            rb3.Visibility = Visibility.Visible;
+                            break;
+                        case 4:
+                            a4.Content = s.Trim('~', '@');
+                            rb4.Visibility = Visibility.Visible;
+                            break;
+                        case 5:
+                            a5.Content = s.Trim('~', '@');
+                            rb5.Visibility = Visibility.Visible;
+                            break;
+                        case 6:
+                            a6.Content = s.Trim('~', '@');
+                            rb6.Visibility = Visibility.Visible;
+                            break;
+                    }
+                    i++;
+                }
+
+            }
+            else 
+            {
+                textA.Visibility = Visibility.Visible;
+                textQ.Text = AQ[1, 0];    
+            }
+        }
+
+
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             openTest ot = new openTest();
             ot.Show();
         }
-
         private void Window_Activated(object sender, EventArgs e)
         {
             
         }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
            
         }
 
+
+
+
         private void Button_Click_1(object sender, RoutedEventArgs e) //Далее
         {
-            A[qNum] = textA.Text;
+
+
+            if (AQ[0, qNum + 1][0] == '@')
+            {
+                if((bool)rb1.IsChecked)
+                {
+                    A[qNum] = "1";
+                }
+                else if ((bool)rb2.IsChecked)
+                {
+                    A[qNum] = "2";
+                }
+                else if ((bool)rb3.IsChecked)
+                {
+                    A[qNum] = "3";
+                }
+                else if ((bool)rb4.IsChecked)
+                {
+                    A[qNum] = "4";
+                }
+                else if ((bool)rb5.IsChecked)
+                {
+                    A[qNum] = "5";
+                }
+                else if ((bool)rb6.IsChecked)
+                {
+                    A[qNum] = "6";
+                }
+                else
+                {
+                    MessageBox.Show("Выберте ответ");
+                    return;
+                }
+
+
+
+
+                textQ.Text = AQ[1, qNum + 1];
+                textA.Visibility = Visibility.Hidden;
+                int i = 0;
+                string[] As = AQ[0, qNum + 1].Split(new char[] { '$' });
+                foreach (string s in As)
+                {
+                    switch (i + 1)
+                    {
+                        case 1:
+                            a1.Content = s.Trim('~', '@');
+                            rb1.Visibility = Visibility.Visible;
+                            break;
+                        case 2:
+                            a2.Content = s.Trim('~', '@');
+                            rb2.Visibility = Visibility.Visible;
+                            break;
+                        case 3:
+                            a3.Content = s.Trim('~', '@');
+                            rb3.Visibility = Visibility.Visible;
+                            break;
+                        case 4:
+                            a4.Content = s.Trim('~', '@');
+                            rb4.Visibility = Visibility.Visible;
+                            break;
+                        case 5:
+                            a5.Content = s.Trim('~', '@');
+                            rb5.Visibility = Visibility.Visible;
+                            break;
+                        case 6:
+                            a6.Content = s.Trim('~', '@');
+                            rb6.Visibility = Visibility.Visible;
+                            break;
+                    }
+                    i++;
+                }
+                i = 0;
+            }
+            else
+            {
+                
+                A[qNum] = textA.Text;
+                label.Content = "№" + (qNum + 2).ToString();
+                bB.IsEnabled = true;
+            }
             qNum++;
-            label.Content = "№" + (qNum+1).ToString();
-            bB.IsEnabled = true;
-            if(qNum == qCount)
+
+
+
+
+            if (qNum == qCount)
             {
                 bN.Content = "Результат";
                 bN.FontWeight = FontWeights.UltraBold;
@@ -127,9 +290,7 @@ namespace Tester
                     tr.Show();
                     this.Hide();
                }
-            }
-            textQ.Text = AQ[1, qNum];
-            textA.Text = A[qNum];
+            }   
         }
 
         private void bB_Click(object sender, RoutedEventArgs e) //Назад
